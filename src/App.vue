de52318ddadf2aca3a6ac1e9058f705795397ba4
cherish_zt <1427730623@qq.com>
2021-04-03 15:00:15 +0800

    添加vuex，vuerouter

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
-  <a-button type="primary">Primary</a-button>
-  <a-button>Default</a-button>
-  <a-button type="dashed">Dashed</a-button>
-  <a-button type="danger">Danger</a-button>
-  <a-button type="primary">按钮</a-button>
-  <a-button type="link">Link</a-button>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
-</script>
 
 <style>
 #app {
@@ -26,6 +13,18 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import App from './App.vue'
 //引入ant-design-vue
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
+import store from './store'
+import router from './router'
 
-const app = createApp(App)
+const app = createApp(App).use(router).use(store)
 //注册ant-design-vue
 app.use(Antd)
 //在id为app的div上，挂载vue根组件
